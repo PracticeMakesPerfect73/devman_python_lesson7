f97948bd838069a3945eaf3172f68502de58635c
Alexey <practicemakesperfect73@gmail.com>
2025-01-21 16:16:37 +0900

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,64 @@
+import ptbot
+from dotenv import load_dotenv
+import os
+from pytimeparse import parse
+
+
+def render_progressbar(total, iteration, prefix='',
+                       suffix='', length=30, fill='█', zfill='░'
+                       ):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+
+
+def notify_progress(secs_left, total_seconds, chat_id, message_id, bot):
+    progress = total_seconds - secs_left
+    progress_bar = render_progressbar(total_seconds,
+                                      progress,
+                                      prefix='',
+                                      suffix='',
+                                      length=30
+                                      )
+    bot.update_message(chat_id,
+                       message_id,
+                       f"Осталось {secs_left} секунд\n{progress_bar}"
+                       )
+
+
+def choose(chat_id, message):
+    bot.send_message(chat_id, message)
+
+
+def countdown_callback(secs_left, total_seconds, chat_id, message_id):
+    notify_progress(secs_left, total_seconds, chat_id, message_id, bot)
+
+
+def main(chat_id, question):
+    seconds = parse(question)
+    message_id = bot.send_message(chat_id, 'Запускаю таймер')
+
+    bot.create_countdown(
+                         seconds,
+                         countdown_callback,
+                         chat_id=chat_id,
+                         message_id=message_id,
+                         bot=bot)
+    bot.create_timer(
+                     seconds,
+                     choose,
+                     chat_id=chat_id,
+                     message="Время вышло!"
+                     )
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    tg_token = os.getenv('TG_TOKEN')
+    tg_chat_id = os.getenv('TG_CHAT_ID')
+    bot = ptbot.Bot(tg_token)
+    bot.reply_on_message(main)
+    bot.run_bot()
